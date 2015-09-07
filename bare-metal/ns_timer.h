868bd55992ada554226eccbd8685dbb426c0faa9
Tero Jääskö <tero.jaasko@arm.com>
2015-09-07 17:08:44 +0300

    event-loop: fix compilation after merge
    The merge from master broke the compilation, as the ns_timer.*
    files are now in two places, one for mbed and another for
    bare-metal/eventOS. Add the changes from commit
    2cc6d3d95fcad2e6285129f3fc80dc763247ee4d to the bare-metal
    side too.
    
    Change-Id: I17f96a7c06567384d1e30e7ce25059bec1856259

--- bare-metal/ns_timer.h
+++ bare-metal/ns_timer.h
@@ -16,7 +16,17 @@
 #ifndef NS_TIMER_H_
 #define NS_TIMER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_CB_TIMER_DIVIDER	1000
+
 extern int8_t ns_timer_init(void);
 extern int8_t ns_timer_sleep(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /*NS_TIMER_H_*/
