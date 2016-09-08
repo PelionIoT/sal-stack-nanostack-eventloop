0f2767b4385b7a8811111417768f53dd60c6df0e
Jaakko Kukkohovi <jaakko.kukkohovi@arm.com>
2016-09-08 12:54:13 +0300

    Enable tick timer through configuration parameter

--- bare-metal/system_timer.c
+++ bare-metal/system_timer.c
@@ -47,7 +47,7 @@ static NS_LIST_DEFINE(system_timer_list, sys_timer_struct_s, link);
 static sys_timer_struct_s *sys_timer_dynamically_allocate(void);
 static void timer_sys_interrupt(void);
 
-#ifndef EVENT_LOOP_USE_PLATFORM_TICK_TIMER
+#ifndef MBED_CONF_NANOSTACK_EVENTLOOP_USE_PLATFORM_TICK_TIMER
 /* Implement platform tick timer using eventOS timer */
 // platform tick timer callback function
 static void (*tick_timer_callback)(void);
